9b84d8da81965a193ebab4bed29d9a5368837710
Jabez Oriel <105244983+jabezoriel@users.noreply.github.com>
2022-07-18 14:23:37 -0300

    Add files via upload

---
+++ Operadores.py
@@ -0,0 +1,19 @@
+"""
+Operadores Aritméticos
+- Subtração
++ Soma
+* Multiplicação
+/ Divisão
+% Resto da Divisão
+** Exponenciação
+// Divisão Inteira ( Arredonda a divisão caso de float )
+() -> dá prioridade a algo
+"""
+print ('Soma' ,10+10)
+print ('Subtração' ,10-10)
+print ('Multiplicação' ,10*10)
+print ('Divisão' ,10/10)
+print ('Resto da Divisão' ,10%10)
+print ('Exponenciação' ,10**10)
+print ('Divisão Inteira' ,10//10)
+print ('.',10+2*(2+10))
